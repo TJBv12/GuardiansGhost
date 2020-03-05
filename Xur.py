7529101ab00381a7cf5e102984ebca15d6743f46
TJBv12 <61827055+TJBv12@users.noreply.github.com>
2020-03-05 11:42:19 +0000

    V0.01
    Initial code to request for Xur's inventory

---
+++ Xur.py
@@ -0,0 +1,13 @@
+import requests
+
+HEADERS = {"X-API-Key":'83bdaf66edb2466fa4ec180b164668ae'}
+xur_url = "https://www.bungie.net/Platform/Destiny/Advisors/Xur/"
+print ("\n\n\nConnecting to Bungie: " + xur_url + "\n")
+print ("Fetching data for: Xur's Inventory!")
+res = requests.get(xur_url, headers=HEADERS)
+
+error_stat = res.json()['ErrorStatus']
+if error_stat == "DestinyVendorNotFound":
+    print("Xur is not currently available!")
+else:
+    print ("Error status: " + error_stat + "\n")
