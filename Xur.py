7599e3a4847b1ac00ad808b4ff4d10a3a25861a1
TJBv12 <61827055+TJBv12@users.noreply.github.com>
2020-03-05 11:54:49 +0000

    Actually works now

--- Xur.py
+++ Xur.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 HEADERS = {"X-API-Key":'83bdaf66edb2466fa4ec180b164668ae'}
 xur_url = "https://www.bungie.net/Platform/Destiny/Advisors/Xur/"
@@ -11,3 +12,7 @@ if error_stat == "DestinyVendorNotFound":
     print("Xur is not currently available!")
 else:
     print ("Error status: " + error_stat + "\n")
+
+input("Press Enter to quit.")
+
+sys.exit()    
